aedfa0f46f3878677e9c64096320ed8caadf57e8
fangweiye <fangweiye@baidu.com>
2024-05-03 23:14:02 +0800

    feat: update version

--- deploy/data.sql
+++ deploy/data.sql
@@ -1,19 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : dev
- Source Server Type    : MySQL
- Source Server Version : 80200
- Source Host           : localhost:3306
- Source Schema         : resource
-
- Target Server Type    : MySQL
- Target Server Version : 80200
- File Encoding         : 65001
-
- Date: 03/05/2024 22:58:44
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
